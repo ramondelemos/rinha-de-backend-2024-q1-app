b3592e09aef3a9207f2f485420fe81a796424f41
Ramon de Lemos <ramondelemos@gmail.com>
2024-02-11 11:11:17 -0300

    chore: Init project

---
+++ lib/rinha_backend.ex
@@ -0,0 +1,18 @@
+defmodule RinhaBackend do
+  @moduledoc """
+  Documentation for `RinhaBackend`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RinhaBackend.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
